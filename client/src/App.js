89a81dc5ac88d7217574b2aa42709a3cabcc1b5f
iamnovik <vlad09082003@mail.ru>
2024-10-15 12:10:54 +0300

    Client side

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BookSearch from "./Components/BookSearch";
+
+const App = () => {
+  return (
+    <div className="App">
+      <BookSearch />
+    </div>
+  );
+};
+
+export default App;
