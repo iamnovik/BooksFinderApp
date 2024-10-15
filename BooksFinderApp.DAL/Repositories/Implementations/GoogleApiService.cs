eaa5c60a5d3b2a11d91f342de7e8560862a359ff
iamnovik <vlad09082003@mail.ru>
2024-10-15 12:27:12 +0300

    API_KEY to .env

--- BooksFinderApp.DAL/Repositories/Implementations/GoogleApiService.cs
+++ BooksFinderApp.DAL/Repositories/Implementations/GoogleApiService.cs
@@ -6,7 +6,8 @@ public class GoogleApiService(HttpClient _client) : IGoogleApiService
 {
     public async Task<string> SearchBooksAsync(string query)
     {
-        var response = await _client.GetAsync($"https://www.googleapis.com/books/v1/volumes?q={query}&key=AIzaSyCRuKv8TpJ9A3VZ4GvFRTZNHiJgUojJeKk");
+        string apiKey = Environment.GetEnvironmentVariable("API_KEY");
+        var response = await _client.GetAsync($"https://www.googleapis.com/books/v1/volumes?q={query}&key={apiKey}");
         response.EnsureSuccessStatusCode();
         return await response.Content.ReadAsStringAsync();
     }
